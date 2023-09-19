3784010689d188eea4954f941649acb08ac697be
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-09-19 12:28:05 +0100

    TK: Made loading playlist of the day simpler

--- src/lib/hooks/fetch_hooks.ts
+++ src/lib/hooks/fetch_hooks.ts
@@ -16,13 +16,20 @@ export async function getPlaylistOfTheDay(access_token: string) {
             "Authorization": `Bearer  ${access_token}`
         },
     },);
-    let playlist_json = await playlist_response.json();
-    let playlists = playlist_json.items;
-    let playlist_names = playlists.map((playlist: any) => playlist.name);
-    let playlist_photo = playlists.map((playlist: any) => playlist.images[0].url);
-    let length = playlist_names.length;
-    let min = 0;
-    let max = length - 1;
-    let randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
-    return {name: playlist_names[randomInt], url: playlist_photo[randomInt]};
+    // IF playlist list is succesfully retrieved, return palylist of the day
+    if (playlist_response.status == 200) {
+        let playlist_json = await playlist_response.json();
+        let playlists = playlist_json.items;
+        let playlist_names = playlists.map((playlist: any) => playlist.name);
+        let playlist_photo = playlists.map((playlist: any) => playlist.images[0].url);
+        let length = playlist_names.length;
+        let min = 0;
+        let max = length - 1;
+        let randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+        return {name: playlist_names[randomInt], url: playlist_photo[randomInt], error: false};
+    // Otherwise return error
+    } else {
+        return {error: true};
+    }
+   
 }
--- src/routes/double_shuffle/+page.server.js
+++ src/routes/double_shuffle/+page.server.js
@@ -1,44 +1,24 @@
 import { CLIENT_ID, CLIENT_SECRET } from '$env/static/private';
 import { getPlaylistOfTheDay } from '$lib/hooks/fetch_hooks';
-import { getTokenFromCode, getTokenFromRefresh } from '../../lib/hooks/auth_hooks';
-import { createClient } from '@supabase/supabase-js'
+import { redirect } from '@sveltejs/kit';
+import { getTokenFromCode } from '../../lib/hooks/auth_hooks';
+// import { createClient } from '@supabase/supabase-js'
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ url, cookies }) {
     // Get access token from cookies
     let access_token = cookies.get('access_token');
-    // Get refresh token from cookies
-    let refresh_token = cookies.get('refresh_token');
-    // If thre is no saved access token, generate new one
+    // If there is no access token. Generate one
     if (access_token == undefined || access_token === '') {
         access_token = await getTokenFromCode(url, cookies, CLIENT_ID, CLIENT_SECRET);
     }
-    // Get user data using access token
-    let user_response = await fetch("https://api.spotify.com/v1/me", {
-        headers: {
-            "Authorization": `Bearer  ${access_token}`
-        },
-    },);
-    // If call succeeds, return users playlists
-    if (user_response.status == 200) {
-        let playlist_names = getPlaylistOfTheDay(access_token);
-        return { playlist: playlist_names };
-        // If call fails due to invalid access token, refresh access token and make call again
-    } else if (user_response.status == 401) {
-        let response = await fetch("https://accounts.spotify.com/api/token",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                    "Authorization": "Basic " + btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)
-                },
-                body: `grant_type=refresh_token&code=${refresh_token}&redirect_uri=http://localhost:5173`
-            });
-        access_token = await getTokenFromRefresh(response, cookies);
-        // Get user data using access token
-        let playlist_names = getPlaylistOfTheDay(access_token);
-        return { playlist: playlist_names };
+    // Get the Playlist
+    let playlist = await getPlaylistOfTheDay(access_token);
+    // If there is an error with playlist retrieval, redirect back to home page.
+    if (playlist.error == true) {
+        throw redirect(302, `/?code=${url.searchParams.get('code')}`);
+    // Else load up the playlist name
     } else {
-        return { playlist: {name: `Failed due to ${user_response.status}`, url: null}}
+        return { playlist: playlist };
     }
 }
