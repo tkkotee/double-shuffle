c063326bdc450b1e1c9f1c30a94bdceab66c385b
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-09-22 13:02:05 +0100

    TK: Added feature cards

--- src/lib/components/FeatureRow.svelte
+++ src/lib/components/FeatureRow.svelte
@@ -1,17 +1,29 @@
 <script>
-	import FeatureCard from "./feature/FeatureCard.svelte";
-
+	import FeatureCard from './feature/FeatureCard.svelte';
 </script>
+
 <div class="row">
-<FeatureCard/>
-<FeatureCard/>
-<FeatureCard/>
+	<FeatureCard
+		title="Drake"
+		img={{ src: 'Drake.webp', alt: 'Drake' }}
+		content="Live follower count from the world’s biggest artist"
+	/>
+	<FeatureCard
+		title="Drake"
+		img={{ src: 'Drake.webp', alt: 'Drake' }}
+		content="Live follower count from the world’s biggest artist"
+	/>
+	<FeatureCard
+		title="Drake"
+		img={{ src: 'Drake.webp', alt: 'Drake' }}
+		content="Live follower count from the world’s biggest artist"
+	/>
 </div>
 
 <style>
-    .row {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-    }
+	.row {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
 </style>
--- src/lib/components/feature/FeatureCard.svelte
+++ src/lib/components/feature/FeatureCard.svelte
@@ -1,13 +1,48 @@
+<script>
+	/**
+	 * @type {string}
+	 */
+	 export let title;
+	/**
+	 * @type {{src: string; alt: string}}
+	 */
+	export let img;
+	/**
+	 * @type {string}
+	 */
+	export let content;
+</script>
+
 <div class="body">
+	<div class="title">{title}</div>
+	<img class="image" src={img.src} alt={img.alt} />
+	<div class="content">{content}</div>
 </div>
 
 <style>
 	.body {
 		background: white;
+		align-items: center;
+		justify-content: start;
 		border-radius: 10px;
-		height: 50vh;
 		width: 25%;
         display: flex;
         flex-direction: column;
+		box-shadow: 0px 3px 18.75px 0px rgba(0, 0, 0, 0.05);
+		padding-top: 5vh;
+		padding-bottom: 5vh;
+		padding-left: 2vw;
+		padding-right: 2vw;
+	}
+
+	.title {
+		font-size: 1.5em;
+	}
+
+	img {
+		height: 15vh;
+		border-radius: 50%;
+		margin-top: 5vh;
+		margin-bottom: 5vh;
 	}
 </style>
