2dcd5168f84967989874af83a1756f2db9bc119d
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-09-24 18:31:33 +0100

    TK: Made potd reading more accurate

--- src/lib/hooks/fetch_hooks.ts
+++ src/lib/hooks/fetch_hooks.ts
@@ -15,24 +15,23 @@ export async function getPlaylists(access_token: string) {
 
 
 export async function getPlaylistOfTheDay(access_token: string, playlist_index: any) {
-    // //TODO: Change length so that it reads the users current #playlists from supabase 
-    // let length = 50;
-    // let int = playlist_index % length;
-    let playlist_response = await fetch("https://api.spotify.com/v1/me/playlists?offset=0&limit=50", {
+    //TODO: Change length so that it reads the users current #playlists from supabase 
+    // Retrieve the relevant playlist from the index stored in supabase
+    let length = 50;
+    let int = playlist_index % length;
+    let playlist_response = await fetch(`https://api.spotify.com/v1/me/playlists?offset=${int}&limit=1`, {
         headers: {
             "Authorization": `Bearer  ${access_token}`
         },
     },);
-    // IF playlist list is succesfully retrieved, return palylist of the day
+    // If playlist list is succesfully retrieved, return palylist of the day
     if (playlist_response.status == 200) {
         let playlist_json = await playlist_response.json();
         let playlists = playlist_json.items;
-        let playlist_names = playlists.map((playlist: any) => playlist.name);
-        let playlist_img_urls = playlists.map((playlist: any) => playlist.images[0].url);
-        let playlist_spotify_urls = playlists.map((playlist: any) => playlist.external_urls.spotify);
-        let length = playlist_names.length;
-        let int = playlist_index % length;
-        return {name: playlist_names[int], img_url: playlist_img_urls[int], spotify_url: playlist_spotify_urls[int], error: false};
+        // Get the first item in the array of playlists. We have 
+        // set limit to 1 so this is just the relevant playlist
+        let playlist = playlists[0];
+        return {name: playlist.name, img_url: playlist.images[0].url, spotify_url: playlist.external_urls.spotify, error: false};
     // Otherwise return error
     } else {
         return {error: true};
